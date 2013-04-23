5df30e51dd009a7df3f6d1b2b6bfd0bd979a4b32
Syohei YOSHIDA <syohex@gmail.com>
2013-04-23 19:49:06 +0900

    Add document for Minilla

--- lib/App/cal.pm
+++ lib/App/cal.pm
@@ -187,3 +187,28 @@ sub _validate {
 }
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+App::cal - calender utiltity for Japanese people
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013 - Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
